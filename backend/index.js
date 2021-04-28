a3627c148b76039e8f86809f3a4fffb9b4cb644b
akremdev <akremdev@gmail.com>
2021-04-29 00:54:49 +0100

    setup routes files

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
 
@@ -14,6 +16,21 @@ mongoose.connect(
         console.log("Connected to MONGODB!");
     }
 )
+
+//middleware 
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+/* app.get("/", (req, res) => {
+    res.send("welcome to homepage")
+})
+app.get("/users", (req, res) => {
+    res.send("welcome to user page")
+}) */
 
 app.listen(8800, () => {
     console.log("Backend server is running!");
